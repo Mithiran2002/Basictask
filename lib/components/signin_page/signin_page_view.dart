f6cace42fccab22ba17223b057875d38a9364edf
Dhananjeyan T G <dqsoft0601@gmail.com>
2024-02-29 12:32:44 +0530

    welcome page corrections done

--- lib/components/signin_page/signin_page_view.dart
+++ lib/components/signin_page/signin_page_view.dart
@@ -1,11 +1,11 @@
-import 'package:gap/gap.dart';
-import 'package:sizer/sizer.dart';
-import 'package:flutter/material.dart';
-import 'package:basic_task/constant.dart';
-import 'package:roundcheckbox/roundcheckbox.dart';
 import 'package:basic_task/components/signin_page/signin_page.dart';
-import 'package:basic_task/components/signup_page/signup_page.dart';
 import 'package:basic_task/components/signin_page/signin_page_view_model.dart';
+import 'package:basic_task/components/signup_page/signup_page.dart';
+import 'package:basic_task/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:roundcheckbox/roundcheckbox.dart';
+import 'package:sizer/sizer.dart';
 
 class SigninPageView extends State<SigninPage> {
   SigninPageViewModel? viewModel;
@@ -16,20 +16,21 @@ class SigninPageView extends State<SigninPage> {
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
   bool isvalid = false;
-   double padValue = 1;
+  double padValue = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor:const Color(0xFFffffff),
+      backgroundColor: const Color(0xFFffffff),
       body: bodyContend(),
     );
   }
-  Widget bodyContend(){
+
+  Widget bodyContend() {
     return Stack(
       children: <Widget>[
         SingleChildScrollView(
-          scrollDirection:Axis.vertical ,
+          scrollDirection: Axis.vertical,
           child: Column(
             children: [
               ClipPath(
@@ -37,161 +38,190 @@ class SigninPageView extends State<SigninPage> {
                 child: Container(
                   height: 30.h,
                   width: 100.w,
-                  decoration:const BoxDecoration(
-                    image: DecorationImage(image: AssetImage("assets/jpg/cloud4.jpg"),fit: BoxFit.cover)
-                  ),  
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/jpg/cloud4.jpg"),
+                          fit: BoxFit.cover)),
                   child: Container(
-                    margin:const EdgeInsets.only(right: 300,bottom: 150),
+                    margin: const EdgeInsets.only(right: 300, bottom: 150),
                     child: IconButton(
-                      onPressed:() {
-                        Navigator.pop(context);
-                      },
-                    icon:  Icon(Icons.arrow_back_ios,size:16.sp,)
-                    ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(
+                          Icons.arrow_back_ios,
+                          size: 16.sp,
+                        )),
                   ),
                 ),
               ),
-               Gap(
+              Gap(
                 2.h,
               ),
-              
-            const Text("Welcome Back",style: TextStyle(fontSize: 35,color:bluegradient,fontWeight: FontWeight.w600),),
-              
-            
-            const Text("Login to Youre Account",style: TextStyle(fontSize: 15,color:color4 ),),
-          
-            
-              Gap(
-                 5.h,
+              const Text(
+                "Welcome Back",
+                style: TextStyle(
+                    fontSize: 35,
+                    color: bluegradient,
+                    fontWeight: FontWeight.w600),
               ),
-            Form(
-              key: formkey,
-              child: Padding(
-                padding:  EdgeInsets.symmetric(horizontal:15.sp,vertical: 6.sp ),
-                child: TextFormField(
-                  validator: (val) => val!.isEmpty || !val.contains("A-Z")
-                              ? "Please Enter the Valid Name"
-                              : null,
-                  decoration:customDecor(
-                    text: "Full Name",
-                    preFixIcon: Icon(Icons.person,color: color6,size: 15.sp,)
+              const Text(
+                "Login to Youre Account",
+                style: TextStyle(fontSize: 15, color: color4),
+              ),
+              Gap(
+                5.h,
+              ),
+              Form(
+                key: formkey,
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 15.sp, vertical: 6.sp),
+                  child: TextFormField(
+                    validator: (val) => val!.isEmpty || !val.contains("A-Z")
+                        ? "Please Enter the Valid Name"
+                        : null,
+                    decoration: customDecor(
+                        text: "Full Name",
+                        preFixIcon: Icon(
+                          Icons.person,
+                          color: color6,
+                          size: 15.sp,
+                        )),
                   ),
-                  
                 ),
               ),
-            ),
-            Gap(
-              2.h,
-            ),
+              Gap(
+                2.h,
+              ),
               Padding(
-                padding:  EdgeInsets.symmetric(horizontal:15.sp,vertical: 6.sp ),
+                padding:
+                    EdgeInsets.symmetric(horizontal: 15.sp, vertical: 6.sp),
                 child: TextFormField(
-                  validator: (value){
-                    if(value!. isEmpty){
+                  validator: (value) {
+                    if (value!.isEmpty) {
                       return "Please enter valid password";
-                    }if(value.length < 8){
+                    }
+                    if (value.length < 8) {
                       return "Must be more than 2 Characters";
                     }
                   },
-                 decoration:customDecor(
-                  text: "password",
-                 ),
-                 obscureText: true,
-                 obscuringCharacter: '●',style: TextStyle(color:color6),
-               ),
+                  decoration: customDecor(
+                    text: "password",
+                  ),
+                  obscureText: true,
+                  obscuringCharacter: '●',
+                  style: TextStyle(color: color6),
+                ),
               ),
-            Row(
-              children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(left: 13.sp),
-                child: RoundCheckBox( 
-                size: 1.5.h, 
-                checkedColor: color6,
-                borderColor: color6,
-                
-                onTap: (selected) {},),
-              ), 
-              Gap(
-                3.w,
+              Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 13.sp),
+                    child: RoundCheckBox(
+                      size: 1.5.h,
+                      checkedColor: color6,
+                      borderColor: color6,
+                      onTap: (selected) {},
+                    ),
+                  ),
+                  Gap(
+                    3.w,
+                  ),
+                  const Text(
+                    "Remember Me",
+                    style: TextStyle(color: color4),
+                  ),
+                  Gap(
+                    30.w,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Forgot password?",
+                      style:
+                          TextStyle(color: color6, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
               ),
-              
-              const Text("Remember Me",style:TextStyle(color: color4),),
-              
               Gap(
-                 30.w,
+                8.h,
               ),
               TextButton(
-                onPressed:() {
-                  
+                onPressed: () {
+                  if (formkey.currentState!.validate()) {
+                    return print(email);
+                  }
                 },
-              
-                  child: const Text("Forgot password?",style: TextStyle(color: color6,fontWeight: FontWeight.bold),),
-
-              )
-              ],
-            ),
-            Gap(
-              8.h,
-            ),
-            TextButton(
-              onPressed: () {
-                if(formkey.currentState!.validate()){
-                 return print(email) 
-                 
-                }
-              },
-              child: Container(
-                height: 5.5.h,
-                width: 70.w,
-                decoration: BoxDecoration(
-                  color: color6,
-                  borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  height: 5.5.h,
+                  width: 70.w,
+                  decoration: BoxDecoration(
+                    color: color6,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: const Center(
+                      child: Text(
+                    "Signin",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  )),
                 ),
-                child:const Center(
-                  child: Text("Signin",style: TextStyle(color: Colors.white,fontSize: 16),)),
               ),
-            ),
-            Gap(
-              2.h,
-            ),
-          
-           Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Don't have an account?",style: TextStyle(fontSize:2.h,color:color4 ),),
-            SizedBox(width: 2.w,),
-            TextButton(
-              onPressed: () {
-                // ignore: non_constant_identifier_names
-                Navigator.push(context, MaterialPageRoute(builder: (Context) => SignUpPage()));
-              },
-              child: Text("Sign up",style: TextStyle(fontSize:2.h,fontWeight: FontWeight.bold,color: bluegradient ),))
-          ],
-                )
-              ],
+              Gap(
+                2.h,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(fontSize: 2.h, color: color4),
+                  ),
+                  SizedBox(
+                    width: 2.w,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        // ignore: non_constant_identifier_names
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (Context) => SignUpPage()));
+                      },
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontSize: 2.h,
+                            fontWeight: FontWeight.bold,
+                            color: bluegradient),
+                      ))
+                ],
+              )
+            ],
           ),
         ),
       ],
     );
   }
 }
-class BottomWaveClipper extends CustomClipper<Path>{
+
+class BottomWaveClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-   var path = new Path();
+    var path = new Path();
     path.lineTo(0.0, size.height - 40);
     path.quadraticBezierTo(
-    size.width / 4, size.height - 80, size.width / 2, size.height - 40);
+        size.width / 4, size.height - 80, size.width / 2, size.height - 40);
     path.quadraticBezierTo(size.width - (size.width / 4), size.height,
-    size.width, size.height - 60);
+        size.width, size.height - 60);
     path.lineTo(size.width, 2.5);
     path.close();
     return path;
   }
 
   @override
-  bool shouldReclip( CustomClipper<Path> oldClipper) {
-   return false;
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
-
 }
--- lib/components/welcome_page/welcome_page.dart
+++ lib/components/welcome_page/welcome_page.dart
@@ -1,82 +1,99 @@
-import 'package:gap/gap.dart';
-import 'package:sizer/sizer.dart';
-import 'package:flutter/material.dart';
-import 'package:basic_task/constant.dart';
-import 'package:basic_task/components/signin_page/signin_page.dart';
 import 'package:basic_task/components/signup_page/signup_page.dart';
 import 'package:basic_task/components/welcome_page/welcome_page_view.dart';
 import 'package:basic_task/components/welcome_page/welcomw_page_view_model.dart';
+import 'package:basic_task/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:sizer/sizer.dart';
+
+import '../signin_page/signin_page.dart';
 
 class WelcomePageView extends State<WelcomePage> {
   WelcomePageViewModel? viewModel;
   WelcomePageView() {
     viewModel = WelcomePageViewModel();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      body: pageContend(),
-     );
+    return SafeArea(
+      child: Scaffold(
+        body: pageContend(),
+      ),
+    );
   }
-  Widget pageContend(){
+
+  Widget pageContend() {
     return Container(
-      height: 100.h,
       width: 100.w,
-      decoration:const BoxDecoration(
-        image: DecorationImage(image: AssetImage("assets/jpg/cloud2.jpg"),fit: BoxFit.cover),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/jpg/cloud2.jpg"), fit: BoxFit.cover),
       ),
       child: Container(
         decoration: BoxDecoration(
-         gradient: LinearGradient(
-          begin: Alignment.bottomRight,
-          end: Alignment.topRight,
-          colors: [
-            darkblue.withOpacity(0.4),
-            darkblue.withOpacity(0.60),
-          ]
-         )
-        ),
-        child: Column(
-          children: <Widget>[
-           Container(
-              // ignore: prefer_const_constructors
-              padding: EdgeInsets.only(left:20,top: 180),
-              width: 100.w,
-              child:const Text("The best \n app for \n City Weather",style: TextStyle(color: Colors.white,fontSize:42,fontWeight: FontWeight.w500),),
-            ),
-            Gap(
-               25.h
-            ),
-            GestureDetector(
-              onTap: () {
-                // ignore: non_constant_identifier_names
-                Navigator.push(context, MaterialPageRoute(builder: (Context) => SigninPage()));
-              },
-              child: Container(
-                height:6.h,
-                width: 80.w,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.sp),
-                  color: Colors.white12,
-                ),
-                child: const Center(
-                  child: Text("Sign in",style:TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.w500)),
+            gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                end: Alignment.topRight,
+                colors: [
+              darkblue.withOpacity(0.4),
+              darkblue.withOpacity(0.60),
+            ])),
+        child: Padding(
+          padding: EdgeInsets.only(left: 5.w, top: 15.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 64.w,
+                child: Text(
+                  "The best app for city weather",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 33.sp,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 2.5,
+                  ),
+                  maxLines: 3,
                 ),
               ),
-            ),
-            Gap(
-             3.h,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => SignUpPage()));
-              },
-              child: 
-                const Text('Create Account',style:TextStyle(color: Colors.white,fontSize: 16)),
-              
-            )
-          ],
+              Gap(25.h),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (Context) => SigninPage()));
+                },
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 13.sp,
+                  ),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                        Colors.blueGrey.withOpacity(0.3)),
+                    minimumSize: MaterialStatePropertyAll(Size(90.w, 6.5.h)),
+                    elevation: const MaterialStatePropertyAll(3),
+                    shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.sp)))),
+              ),
+              Gap(2.h),
+              Align(
+                alignment: Alignment.center,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()));
+                  },
+                  child: Container(
+                    child: Text('Create an account',
+                        style: TextStyle(color: Colors.white, fontSize: 11.sp)),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
